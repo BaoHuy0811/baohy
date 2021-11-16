ecc0e1c314a50ff110a82828af6792d60a371897
BaoHuy0811 <94435376+BaoHuy0811@users.noreply.github.com>
2021-11-16 22:57:20 +0700

    upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Ủa làm j zô đâyy!',
+     
+    btnIntro: 'thík',
+    title: 'Hỏi câu này cái đượt hok 😙',
+    desc: 'Em bíc nấu cơm hok? Bíc thì zề nấu cơm chó zs anh nek',
+    btnYes: 'Dạ!',
+    btnNo: 'Hok',
+    question:'Z e có nguyện cùng anh nấu cơm suốt cuộc đời này luôn hok    ',
+    btnReply: 'Send to me ',
+    reply: 'Muốn thì oke hok muốn cũng oke    ',
+    mess: 'Yay! Em chính thức thành người yêu tui rùi đoáaa 🥰. Anh yêu bé 😘😘',
+    messDesc: 'Ngần ngại gì mà hok nhắn tin zới anh đy.',
+    btnAccept: 'Dạ!',
+    messLink: 'https://www.messenger.com/t/100017405381737' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
+
